f81e25560bd70351bb4fd366ae5c4d97e172b6ed
wangw <abc123456>
2026-03-18 16:31:53 +0800

    新增deepseek对话接口

--- SpringAIQuickStart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
+++ SpringAIQuickStart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
@@ -1,11 +1,28 @@
 package com.example.springaiquickstart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+
 /**
  * @Author ww
  * @PackageName ai_agent
  * @Package com.example.springaiquickstart.controller
  * @Version 1.0
  */
+@RestController
+@RequestMapping("/ai")
 public class ChatController {
+    @Autowired
+    private DeepSeekChatModel deepSeekChatModel;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(value = "message",defaultValue = "你是谁？") String message){
+        String resp = deepSeekChatModel.call(message);
+        return resp;
+    }
 
 }
