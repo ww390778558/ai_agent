7b48eb56d5a2e02b3522ddc8d400d76f014b6697
wangw <abc123456>
2026-03-18 18:09:36 +0800

    优化前端交互方式, 调整为流式输出, 体验更友好

--- SpringAIQuickStart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
+++ SpringAIQuickStart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
@@ -1,11 +1,16 @@
 package com.example.springaiquickstart.controller;
 
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import reactor.core.publisher.Flux;
 
 /**
  * @Author ww
@@ -25,4 +30,17 @@ public class ChatController {
         return resp;
     }
 
+
+    /**
+     *  流式输出
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/chatStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chatStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        Flux<ChatResponse> stream = deepSeekChatModel.stream(prompt);
+        // SSE: 每个元素会作为一个 data: ... event 推送给前端
+        return stream.map(resp -> resp.getResult().getOutput().getText());
+    }
 }
