9fdf891be3c10be30cd901c57cbcdd5eeb9c60d6
wangw <abc123456>
2026-03-18 16:08:13 +0800

    初始化项目

---
+++ SpringAIQuickStart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.example.springaiquickstart.controller;
+
+/**
+ * @Author ww
+ * @PackageName ai_agent
+ * @Package com.example.springaiquickstart.controller
+ * @Version 1.0
+ */
+public class ChatController {
+
+}
